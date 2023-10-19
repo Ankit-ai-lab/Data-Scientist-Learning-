a1174bf7e8d3cd0fe618cd10fd461b3929ac07de
Ankit Gupta <134631639+Ankit-ai-lab@users.noreply.github.com>
2023-10-19 14:48:04 +0530

    Add files via upload

---
+++ Opperators (2).py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+# coding: utf-8
+'''Operators 
+Arithmetic operator
+Relational Operator
+Assignment Operator
+• Logical operator 
+Membership Operator
+
+# In[1]:
+
+
+a = 50
+b = 50
+print(b*a)
+
+
+# In[2]:
+
+
+# Arithmetric Operators
+a = 5
+print(a*6)
+
+print(a-6)
+
+print(5*6)
+
+print(5/2)
+
+print(5//2)
+
+print(5%2)
+
+print(5**3)
+
+
+# In[3]:
+
+
+a = 5
+b = 3
+print(a**b)
+
+
+# In[4]:
+
+
+# Relational Operators
+print(4>5)
+ 
+    
+print(4<5)
+ 
+print(4>=4)
+
+print(4<=4)
+
+print(4==4)
+
+print(4!=5)
+
+
+# In[5]:
+
+
+print(8<7)
+
+
+# In[ ]:
+
+
+
+
+
+# In[6]:
+
+
+# Logical Operators
+print(700 and 750)  
+
+print(8   or 0 )
+
+print(not 1)
+
+
+# In[7]:
+
+
+# Assignment Operators
+
+a =- 8
+print(a)
+
+
+
+# In[8]:
+
+
+# Membership Operators
+
+# in/not in
+
+print('o' in 'omadrash')
+
+print(6 not in [1,12,3,4,5,6])
+#print(54 is not  [1,12,3,4,5,6])
+
+
+# In[10]:
+
+
+x = 6
+
+if x > 5:
+    print("x is greater than 5")
+    
+    if x > 8:
+        print("x is also greater than 8")
+    else:
+        print("x is not greater than 8")
+else:
+    print("x is not greater than 5")
+
+
+# In[13]:
+
+
+####Simple If else statement
+print("EX.1) SIMPLE IF ELSE STATEMENT")
+x = int(input("Enter number: "))
+print("You have entered: ", x)
+if x >=  20:
+    print("OK")
+else:
+    print("Not OK")
+
+
+# In[ ]:
+
+
+a =(input("Enter any number: "))
+
+
+# # 7. Identity Operators:
+# These are used to compare the memory locations of two objects.
+
+# In[ ]:
+
+
+x = 5
+y = 5
+
+# Identity operators
+is_same_object = x is y
+is_not_same_object = x is not y
+
+# Print the results
+print("is_same_object:", is_same_object)
+print("is_not_same_object:", is_not_same_object)
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
